8033c3fbed6ef6f43e6fed82b0de1aa3e277334c
@n.ponomarev-78506 <niiuponomarev@edu.hse.ru>
2026-01-14 14:50:52 +0300

    feat(dependencies): add kotlinx.datetime and kotlinx.coroutines.test libraries

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -42,9 +42,11 @@ kotlin {
             implementation(libs.androidx.lifecycle.runtimeCompose)
             implementation(libs.kotlinx.coroutines.core)
             implementation(libs.kotlinx.serialization.json)
+            implementation(libs.kotlinx.datetime)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
+            implementation(libs.kotlinx.coroutines.test)
         }
     }
 }
